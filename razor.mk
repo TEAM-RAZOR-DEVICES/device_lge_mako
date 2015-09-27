409e7006f0694b931f7ba96456f58171f64b6156
vibhu0009 <cvibhu0009@gmail.com>
2015-09-27 12:43:42 +0200

    Razor bringup

--- razor.mk
+++ razor.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/razor/config/gsm.mk)
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common Razor stuff.
+$(call inherit-product, vendor/razor/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/razor/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
@@ -19,7 +19,7 @@ PRODUCT_RELEASE_NAME := mako
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := slim_mako
+PRODUCT_NAME := razor_mako
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
